8b5d4b5780cca5245847add74a9225405e7245ce
pierremegevand <pierre.megevand@gmail.com>
2024-02-27 12:54:30 +0100

    1st version compatible with Voxeloc / BIDS
    Major update to make the function compatible with Voxeloc's BIDS output.

--- SEEG2parc.m
+++ SEEG2parc.m
@@ -1,35 +1,40 @@
-function elecInfo=SEEG2parc(fsSubj,cfg)
+function elecInfo=SEEG2parc(cfg)
 
-% function elecInfo=SEEG2parc(fsSubj,cfg)
+% function elecInfo=SEEG2parc(cfg)
 % Attribute cerebral parcellation labels to stereo-EEG electrodes.
+%
+% In its current version, the function is compatible both with iELVis and
+% with the BIDS output of Voxeloc. In order to identify the folder where
+% the relevant information is, it first opens a system UI box so that the
+% user directs to the correct folder.
 %
 % Inputs:
 %
-%   fsSubj:         (string) name of the FreeSurfer subject
+%   cfg:            (structure) configuration strucure with the following
+%                   fields:
 %
-%   cfg:            (structure) optional configuration strucure with the 
-%                   following fields (all optional):
+%     dataType:     (string) either 'iELVis' or 'BIDS'
 %
 %     parcType:     (string) either 'DK' (default) for the Desikan-Killiany
 %                   parcellation or 'Des' for the Destrieux parcellation
 %
-%     tissueType:   (string) 'cortex' if only cortical tissue 
+%     tissueType:   (string) 'cortex' if only cortical tissue
 %                   hippocampus and amygdala) is to be considered,
-%                   otherwise (default) all FreeSurfer parcellation labels 
+%                   otherwise (default) all FreeSurfer parcellation labels
 %                   are used
 %
 % Outputs:
 %
-%   elecInfo:       N-by-6 cell array, where N is the number of electrodes.
+%   elecInfo:       N-by-7 cell array, where N is the number of electrodes.
 %
-%     column 1:     electrode name (from iELVis)
+%     column 1:     electrode name
 %
-%     column 2:     electrode type (from iELVis; 'D' for depth electrode)
+%     column 2:     electrode type ('D' or 'depth')
 %
-%     column 3:     hemisphere (from iELVis)
+%     column 3:     hemisphere ('L' or 'R')
 %
-%     column 4:     electrode coordinates, in patient space, in LIP
-%                   coordinates (fom iELVis)
+%     column 4:     electrode coordinates, in patient space, in ILA
+%                   coordinates
 %
 %     column 5:     tissue label(s), deterministic; returns 'unknown' if no
 %                   valid tissue label was found near the electrode
@@ -38,7 +43,7 @@ function elecInfo=SEEG2parc(fsSubj,cfg)
 %                   label, column 6 is equal to column 5; returns 'unknown'
 %                   if no valid tissue label was found near the electrode
 %
-%     column 7:     weight of each tissue label in column 6 ([] if tissue 
+%     column 7:     weight of each tissue label in column 6 ([] if tissue
 %                   label is 'unknown')
 %
 % The attribution of tissue labels is based on a 3x3x3-voxel cube centered
@@ -59,7 +64,7 @@ function elecInfo=SEEG2parc(fsSubj,cfg)
 % |        |        |        | /
 % |        |        |        |/
 % +--------+--------+--------+
-% 
+%
 %          +--------+--------+--------+
 %         /    _   /        /    _   /|
 %        /   \/2  /    1   /   \/2  / |
@@ -75,7 +80,7 @@ function elecInfo=SEEG2parc(fsSubj,cfg)
 % |        |        |        | /
 % |        |        |        |/
 % +--------+--------+--------+
-% 
+%
 %          +--------+--------+--------+
 %         /    _   /    _   /    _   /|
 %        /   \/3  /   \/2  /   \/3  / |
@@ -91,21 +96,13 @@ function elecInfo=SEEG2parc(fsSubj,cfg)
 % |        |        |        | /
 % |        |        |        |/
 % +--------+--------+--------+
-% 
+%
 %
 % https://github.com/HumanNeuronLab/SEEG2parc
 % Pierre Mégevand, Human Neuron Lab, University of Geneva, Switzerland. 2022-2024.
 % pierre.megevand@unige.ch; https://www.unige.ch/medecine/neucli/en/groupes-de-recherche/1034megevand/
 
-
-
-global globalFsDir;
-
-% parse inputs
-if nargin==1
-    cfg=[];
-end
-
+if ~isfield(cfg,'dataType'), error('Please specify data type (iELVis or BIDS).'); end
 if ~isfield(cfg,'parcType'), parcType='DK'; else, parcType=cfg.parcType; end
 if ~isfield(cfg,'tissueType'), tissueType='all'; else, tissueType=cfg.tissueType; end
 
@@ -120,20 +117,53 @@ switch parcType
         error('I do not recognize this parcellation: %s.',parcType);
 end
 
-elecNames=readiELVisElecNames(fsSubj);
-nElec=size(elecNames,1);
+filepath=uigetdir([],'Select patient folder');
+[~,patID]=fileparts(filepath);
 
-% load volume parcellations (ILA)
-wmparc=MRIread(fullfile(globalFsDir,fsSubj,'mri','wmparc.mgz')); % load white matter parcellation
-parcVol=MRIread(fullfile(globalFsDir,fsSubj,'mri',myParcVol)); % load volume parcellation
-parcVol.vol(parcVol.vol==2|parcVol.vol==41)=wmparc.vol(parcVol.vol==2|parcVol.vol==41); % replace white matter labels with DK white matter parcellation
 
-% load electrode coordinates (LIP)
-elecCoord=readiELVisElecCoord(fsSubj,'LEPTOVOX');
+switch cfg.dataType
+    
+    case 'iELVis'
+        
+        % load electrode names
+        elecNames=readiELVisElecNames(patID);
+        elecType=elecNames(:,2);
+        elecHem=elecNames(:,3);
+        elecNames=elecNames(:,1);
+        
+        % load electrode coordinates (LIP)
+        elecCoord=readiELVisElecCoord(patID,'LEPTOVOX');
+        
+        % load volume parcellations (ILA)
+        wmparc=MRIread(fullfile(filepath,'mri','wmparc.mgz')); % load white matter parcellation
+        parcVol=MRIread(fullfile(filepath,'mri',myParcVol)); % load volume parcellation
+        parcVol.vol(parcVol.vol==2|parcVol.vol==41)=wmparc.vol(parcVol.vol==2|parcVol.vol==41); % replace white matter labels with DK white matter parcellation
+        
+    case 'BIDS'
+        
+        % load electrode table
+        elecTable=readtable(fullfile(filepath,'ieeg',[patID '_electrodes.tsv']),'FileType','text','Delimiter','\t','ReadVariableNames',true,'ReadRowNames',false);
+        elecNames=elecTable.name;
+        elecType=elecTable.type;
+        elecHem=elecTable.hemisphere;
+        
+        % voxeloc coords are LIA
+        elecCoord=table2array(elecTable(:,2:4));
+        elecCoord(:,3)=256-elecCoord(:,3); % make them LIP like iELVis
+        
+        % load volume parcellations (ILA)
+        wmparc=MRIread(fullfile(filepath,'anat',[patID '_wmparc.mgz'])); % load white matter parcellation
+        parcVol=MRIread(fullfile(filepath,'anat',[patID '_' myParcVol])); % load volume parcellation
+        parcVol.vol(parcVol.vol==2|parcVol.vol==41)=wmparc.vol(parcVol.vol==2|parcVol.vol==41); % replace white matter labels with DK white matter parcellation
+end
+
+nElec=size(elecCoord,1);
 
 % start populating elecInfo
-elecInfo=cell(nElec,6);
-elecInfo(:,1:3)=elecNames;
+elecInfo=cell(nElec,7);
+elecInfo(:,1)=elecNames;
+elecInfo(:,2)=elecType;
+elecInfo(:,3)=elecHem;
 elecInfo(:,4)=num2cell(elecCoord,2); % storing the original coordinates (LIP)
 
 % match elecCoord dimension order and direction with scans
@@ -155,7 +185,7 @@ voxelWeights=1/cat(3, ...
     [sqrt(3) sqrt(2) sqrt(3); sqrt(2) 1 sqrt(2); sqrt(3) sqrt(2) sqrt(3)]);
 
 for ctElec=1:nElec
-    if strcmp(elecNames{ctElec,2},'D') % double-check that this is a depth electrode
+    if strncmpi(elecType{ctElec},'D',1) % double-check that this is a depth electrode
         
         voxelCubeCoord=[ ...
             round(elecCoord(ctElec,1))-1, round(elecCoord(ctElec,2))-1, round(elecCoord(ctElec,3))-1; ...
@@ -229,17 +259,42 @@ end
 
 %% plotting (useful for debug)
 
-% CT=MRIread(fullfile(globalFsDir,fsSubj,'elec_recon','postInPre.nii'));  % load post-implant CT
-% dummyElecCoord=round(elecCoord(8,:));
-% figure;
-% colormap bone;
-% subplot(1,3,1);
-% imagesc(squeeze(CT.vol(dummyElecCoord(1),:,:)));
-% axis image;
-% subplot(1,3,2);
-% imagesc(squeeze(CT.vol(:,dummyElecCoord(2),:)));
-% axis image;
-% subplot(1,3,3);
-% imagesc(squeeze(CT.vol(:,:,dummyElecCoord(3))));
-% axis image;
-
+%{
+switch cfg.dataType
+    case 'iELVis'
+        CT=MRIread(fullfile(filepath,'elec_recon','postInPre.nii'));  % load post-implant CT
+    case 'BIDS'
+        CT=MRIread(fullfile(filepath,'anat',[patID '_postInPre.nii'])); % load post-implant CT
+end
+elecCoordRound=round(elecCoord);
+figure;
+for ctElec=1:size(elecCoord,1)
+    colormap bone;
+    subplot(1,3,1);
+    imagesc(squeeze(CT.vol(elecCoordRound(ctElec,1),:,:)));
+    hold all;
+    scatter(elecCoordRound(ctElec,3),elecCoordRound(ctElec,2),'MarkerEdgeColor',[1 1 0],'MarkerFaceColor',[1 1 0],'MarkerFaceAlpha',0.3);
+    hold off;
+    axis image;
+    xlabel('P to A');
+    ylabel('R to L');
+    subplot(1,3,2);
+    imagesc(squeeze(CT.vol(:,elecCoordRound(ctElec,2),:)));
+    hold all;
+    scatter(elecCoordRound(ctElec,3),elecCoordRound(ctElec,1),'MarkerEdgeColor',[1 1 0],'MarkerFaceColor',[1 1 0],'MarkerFaceAlpha',0.3);
+    hold off;
+    axis image;
+    xlabel('P to A');
+    ylabel('S to I');
+    title(elecNames{ctElec});
+    subplot(1,3,3);
+    imagesc(squeeze(CT.vol(:,:,elecCoordRound(ctElec,3))));
+    hold all;
+    scatter(elecCoordRound(ctElec,2),elecCoordRound(ctElec,1),'MarkerEdgeColor',[1 1 0],'MarkerFaceColor',[1 1 0],'MarkerFaceAlpha',0.3);
+    hold off;
+    axis image;
+    xlabel('R to L');
+    ylabel('S to I');
+    pause;
+end
+%}
